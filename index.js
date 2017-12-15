0195907b684ff544b799ffb04ec4fbc8842dfbb9
Andres Rojas <andres.rojas.isaza@gmail.com>
2017-12-15 11:22:21 -0500

    Added logic for second version, app as an API

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
-const request = require('request');
-const _ = require('lodash');
-const csv=require('csvtojson');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const csv = require('csvtojson');
+const alasql = require('alasql');
+const moment = require('moment');
+alasql.fn.moment = moment;
 
-const key = '';
-const csvFilePath='./data.csv';
-const  events = [];
+const csv2json = (filename) => {
+  return new Promise((success, reject) => {
+    let csv_data = [];
 
-csv().fromFile(csvFilePath) .on('json',(jsonObj) =>{
-    events.push(jsonObj);
-})
-.on('done',(error)=> {
-    const dataString = 'api_key='+ key + '&event=' + JSON.stringify(_.slice(events, [0], [1000]));
-    const options = {
-        url: 'https://api.amplitude.com/httpapi',
-        method: 'POST',
-        body: dataString
-    };
-    request(options,(error, response, body) => {
-        if (error) {
-          return console.error('upload failed:', error);
+    // Transform CSV into JSON
+    csv()
+      .fromFile(`./data/${filename}`)
+      .on('json',(jsonObj)=>{
+        csv_data.push({
+          distinct_id: jsonObj.distinct_id,
+          time: moment(parseInt(jsonObj.time)).startOf('day') // Set it to the beginning of the day
+        });
+      })
+      .on('done',(error)=>{
+        if(error) {
+          reject(error);
+        } else {
+          success(csv_data);
         }
-        console.log('Upload successful!  Server responded with:', body);
-    });
-})
+      })
+  });
+};
 
 
+// Body parser configuration
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+// Initial API Endpoint
+app.get('/', (req, res) => {
+  res.json({ api: 'V1.0', description: 'Cohorts API'});
+});
+
+app.post('/cohort', (req, res) => {
+  const { filename, frecuency } = {filename: 'effortless.csv', frecuency: 'weekly'};
+
+  csv2json(filename)
+    .then((data) => {
+      // Order the response by the date from older to newer
+      const response = alasql('SELECT DISTINCT distinct_id, time from ? ORDER BY time ASC', [data]);
+      res.json(response);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Something broke!');
+    })
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
